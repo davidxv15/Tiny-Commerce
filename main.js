1856cb82bd642a21954157020da18a2854750ccb
davidxv15 <davidxvaz@gmail.com>
2025-04-03 22:35:23 -0700

    feat(main): flex

--- main.js
+++ main.js
@@ -5,13 +5,17 @@ fetch('./data/products.json')
     products.forEach(product => {
       const card = document.createElement('div');
       card.className = 'product-card';
+
       card.innerHTML = `
+      <div class="card-content">
         <img src="${product.image}" alt="${product.name}" />
         <h3>${product.name}</h3>
         <p>${product.description}</p>
         <p><strong>$${product.price.toFixed(2)}</strong></p>
         <button class="add-to-cart">Add to Cart</button>
+        </div>
       `;
+      
       list.appendChild(card);
     });
   })
