d65fd67b5fd580e35dd01aaf2faafb7da8be5a0e
davidxv15 <davidxvaz@gmail.com>
2025-04-07 22:54:00 -0700

    refactor(cart): structure: cart stub

---
+++ cart.js
@@ -0,0 +1,14 @@
+fetch('.data/products.json')
+ .then(res => res.json())
+ .then(products => {
+    const params = new URLSearchParams(window.location.search);
+    const id = parseInt(params.get("id"));
+
+    if (!product) {
+        document.body.innerHTML = "<p>No items in cart</p>";
+        return;
+    }
+
+    
+
+ });
