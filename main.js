650087a0e70df1619a6a3e14cffdbf9a1efeb746
davidxv15 <davidxvaz@gmail.com>
2025-04-15 19:10:30 -0700

    refactor(main.js): <p id=product-price class=price>23826{product.price.toFixed(2)}</p>

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ fetch('./data/products.json')
           <img src="${product.image}" alt="${product.name}" />
           <h3><a href="product.html?id=${product.id}">${product.name}</a></h3>
           <p>${product.description}</p>
-          <p><strong>$${product.price.toFixed(2)}</strong></p>
+          <p id="product-price" class="price">$${product.price.toFixed(2)}</p>
+
           <button class="add-to-cart" data-id="${product.id}">Add to Cart</button>
         </div>
       `;
